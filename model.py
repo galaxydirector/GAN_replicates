ac0b78b6e560f5c5ff4aae6bae50dc0f579134ba
galaxydirector <zhangy8665@gmail.com>
2019-04-18 15:42:31 -0400

    init architecture

---
+++ model.py
@@ -0,0 +1,29 @@
+# this file is to save the GAN model
+# operations would be saved as functions, otherwise to be class methods
+
+
+
+class GAN_fc:
+	def __init__(self):
+
+
+
+
+
+	def create_varaibles(self):
+
+
+
+
+	def create_generator(self):
+
+
+	def create_discriminator(self):
+
+
+	def create_model(self):
+
+
+	def loss(self):
+
+		
---
+++ train.py
@@ -0,0 +1,16 @@
+# use to train
+"""
+This file controls the operation to train a model
+The pipeline starts with 
+1.feed in data
+2.perprocess it
+3.feed 'em into the model, set up training params
+4.and save them properly, record all the training data properly and possibly display 'em
+"""
+
+
+
+def create_data_batch():
+
+	yield
+
